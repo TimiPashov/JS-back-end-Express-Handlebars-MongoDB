94f73af693002ae72b1becea50ac9586c2eea909
Timi Pashov <122820612+TimiPashov@users.noreply.github.com>
2024-03-07 10:59:56 +0200

    Add files via upload

---
+++ Tutorials/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// TODO change DB name according to assingment
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/Tutorials'
+
+module.exports = async (app)=>{
+    try{
+        mongoose.connect(CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database connected');
+    }catch(error){
+        console.log(error.message);
+        process.exit(1);
+    }
+};
